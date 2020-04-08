97125d61db230802121db674fe1b839833ec15cb
Patrick Fernandes <patrick.fernandes@city.ac.uk>
2020-04-08 20:46:41 +0100

    waterbuffalo css-grid

--- wms2B/graph.py
+++ wms2B/graph.py
@@ -1,18 +1,112 @@
 import dash_core_components as dcc
 import plotly.figure_factory as ff
+import plotly.graph_objects as go
 
-piechart = dcc.Graph(
-    id="pie_chart",
-    figure={
-        'data': [
-            {'values': [8, 10, 2, 4], 'labels': ["sleep", "work", "recreation", "projects"], 'type': 'pie'}],
-    }
+
+labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
+values = [4500, 2500, 1053, 500]
+
+pie = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+pie.update_layout(
+    autosize=True,
+    # width=500,
+    height=300,
+    margin=dict(
+        l=50,
+        r=50,
+        b=50,
+        t=50,
+
+    ),
+    # paper_bgcolor="LightSteelBlue",
 )
+pie.update_layout(showlegend=False)
 
 df = [dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28'),
       dict(Task="Job B", Start='2009-03-05', Finish='2009-04-15'),
       dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30')]
 
-fig = ff.create_gantt(df)
-fig["layout"].pop("height", None)
-fig["layout"].pop("width", None)
+gantt_diagram = ff.create_gantt(df)
+# gantt_diagram["layout"].pop("height", None)
+gantt_diagram["layout"].pop("width", None)
+
+sleep = go.Figure()
+sleep.add_trace(go.Bar(
+    y=['sleep'],
+    x=[20],
+    name='SF Zoo',
+    orientation='h',
+    marker=dict(
+        color='rgba(246, 78, 139, 0.6)',
+        line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+    )
+))
+sleep.add_trace(go.Bar(
+    y=['sleep'],
+    x=[12],
+    name='LA Zoo',
+    orientation='h',
+    marker=dict(
+        color='rgba(58, 71, 80, 0.6)',
+        line=dict(color='rgba(58, 71, 80, 1.0)', width=3)
+    ),
+))
+
+sleep.update_layout(barmode='stack')
+
+
+fig = go.Figure()
+
+fig.add_trace(go.Indicator(
+    mode = "number+gauge+delta", value = 180,
+    delta = {'reference': 200},
+    domain = {'x': [0.25, 1], 'y': [0.08, 0.25]},
+    title = {'text': "Revenue"},
+    gauge = {
+        'shape': "bullet",
+        'axis': {'range': [None, 300]},
+        'threshold': {
+            'line': {'color': "black", 'width': 2},
+            'thickness': 0.75,
+            'value': 170},
+        'steps': [
+            {'range': [0, 150], 'color': "gray"},
+            {'range': [150, 250], 'color': "lightgray"}],
+        'bar': {'color': "black"}}))
+
+fig.add_trace(go.Indicator(
+    mode = "number+gauge+delta", value = 35,
+    delta = {'reference': 200},
+    domain = {'x': [0.25, 1], 'y': [0.4, 0.6]},
+    title = {'text': "Profit"},
+    gauge = {
+        'shape': "bullet",
+        'axis': {'range': [None, 100]},
+        'threshold': {
+            'line': {'color': "black", 'width': 2},
+            'thickness': 0.75,
+            'value': 50},
+        'steps': [
+            {'range': [0, 25], 'color': "gray"},
+            {'range': [25, 75], 'color': "lightgray"}],
+        'bar': {'color': "black"}}))
+
+fig.add_trace(go.Indicator(
+    mode = "number+gauge+delta", value = 220,
+    delta = {'reference': 200},
+    domain = {'x': [0.25, 1], 'y': [0.7, 0.9]},
+    title = {'text' :"Satisfaction"},
+    gauge = {
+        'shape': "bullet",
+        'axis': {'range': [None, 300]},
+        'threshold': {
+            'line': {'color': "black", 'width': 2},
+            'thickness': 0.75,
+            'value': 210},
+        'steps': [
+            {'range': [0, 150], 'color': "gray"},
+            {'range': [150, 250], 'color': "lightgray"}],
+        'bar': {'color': "black"}}))
+# fig.update_layout(height = 400 , margin = {'t':0, 'b':0, 'l':0})
+
+
--- wms2B/index.py
+++ wms2B/index.py
@@ -9,19 +9,29 @@ index_page = html.Div([
 
         html.Div([
 
-            html.Div([piechart], className = "box1"),
-            html.Div([dcc.Graph(figure=fig, id='gantt-id')], className = "box2"),
+            html.Div([
+                dcc.Graph(figure=pie)
+            ], className = "box1"),
+
+            html.Div([
+                 dcc.Graph(figure = gantt_diagram)
+            ], className = "box2"),
+
             html.Div(["Box3"], className = "box3"),
             html.Div(["Box4"], className = "box4"),
             html.Div(["Box5"], className = "box5"),
 
             html.Div(["Box6"], className="box6"),
-            html.Div(["Box7"], className="box7"),
+            html.Div([
+               # dcc.Graph(figure=sleep)
+            ], className="box7"),
             html.Div(["Box8"], className="box8"),
             html.Div(["Box9"], className="box9"),
             html.Div(["Box10"], className="box10"),
 
-            html.Div(["Box11"], className="box11"),
+            html.Div([
+                # dcc.Graph(figure=fig)
+            ], className="box11"),
             html.Div(["Box12"], className="box12"),
             html.Div(["Box13"], className="box13"),
             html.Div(["Box14"], className="box14"),
